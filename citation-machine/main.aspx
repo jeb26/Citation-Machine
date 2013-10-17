55e779916b6037cb0ba4bc554fc46bc1dfceaad9
James Edward Bell <bell.edward.james@gmail.com>
2013-10-17 01:42:30 -0400

    init commit

---
+++ citation-machine/main.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="ParseXML.main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    This is iA going to become the Citation Machine... <br /><br />
+
+    Welcome to the Citation Machine!<br /><br />
+       
+    ISBN-10: <asp:TextBox ID="isbn" runat="server"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+    <br /><br />Output from server: <br /><br /> <%=t %>
+           
+    </div>
+    </form>
+</body>
+</html>
---
+++ citation-machine/main.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace ParseXML
+{
+
+    public partial class main : System.Web.UI.Page
+    {
+        protected string myVar = "hello from the server!";
+        protected string t;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            t = getNodes("0887307280");
+        }
+
+        protected string getNodes(string isbn)
+        {
+            string titles = "";
+            string url = "http://www.worldcat.org/webservices/catalog/search/opensearch?q="+isbn+"&wskey=fRDCboLGRMyPgWPEXNT6ybLDe9ZySdp1VH9SxWDMxiyjFRTHc2ATZVTYbrGgXwXEsM4hQozz3lPapR3R";
+            //string url1 = "file:///C:/Users/Administrator/Desktop/prog/xml/worldCat.xml";
+
+            XmlDocument d = new XmlDocument();
+            d.Load(url);
+            XmlNodeList n = d.GetElementsByTagName("title");
+
+            for (int i = 0; i < n.Count; i++ )
+            {
+                titles += n[i].InnerText + "<br />";
+            }
+
+            return titles;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
